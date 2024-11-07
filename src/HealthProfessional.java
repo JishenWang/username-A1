49b7d99ce716ca341c173e9ff07e506919bf5680
JishenWang <1801076890@qq.com>
2024-11-07 14:28:33 +0800

    1
    Creates a base class setting instance variable id, name, profession. Use two constructors and initialize all instance variables, and use methods that print instance variables

--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -1,3 +1,27 @@
 public class HealthProfessional {
+    private final int id;
+    private final String name;
+    private final String profession ;
+
+    // Default Constructor
+    public HealthProfessional() {
+        this.id = 0;
+        this.name = "Indeterminacy";
+        this.profession = "Unknown";
+    }
+
+    // Constructor with parameters
+    public HealthProfessional(int id, String name, String profession) {
+        this.id = id;
+        this.name = name;
+        this.profession = profession;
+    }
+
+    // Method to print instance variables
+    public void printDetails() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Profession: " + profession);
+    }
 
 }
