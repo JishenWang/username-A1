6f8a6998a70fb706b32823d61a91f7d3064ccd13
JishenWang <1801076890@qq.com>
2024-11-07 14:54:07 +0800

    2
    Create the GeneralPractitioner subclass that inherits the base class: HealthProfessional and adds an instance variable that distinguishes between GPS and surgeons: surgeryType
    A default constructor and a constructor that initializes all instance variables, with the Override method added for output.

--- src/GeneralPractitioner.java
+++ src/GeneralPractitioner.java
@@ -1,4 +1,23 @@
-public class GeneralPractitioner {
-    
+public class GeneralPractitioner extends HealthProfessional{
+    private final String  surgeryType;
+
+    // Constructor
+    public GeneralPractitioner() {
+        super();
+        this.surgeryType = "Default surgeryType";
+    }
+
+    // Constructor with parameters
+    public GeneralPractitioner(int id, String name, String profession, String surgeryType) {
+        super(id, name, profession);
+        this.surgeryType = surgeryType;
+    }
+
+    // Override printDetails method
+    @Override
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("surgeryType: " + surgeryType);
+    }
 
 }
