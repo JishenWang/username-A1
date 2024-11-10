62acf3e159b549be41e5e02e3aaf999876796c02
JishenWang <1801076890@qq.com>
2024-11-10 16:10:45 +0800

    3
    Creating the Surgeon class and inheriting the HealthProfessional class to differentiate between changing a new instance of GeneralPractitioner to clinic and a new instance of Surgeon to surgeryType, Use Getter and Setter methods

--- src/Surgeon.java
+++ src/Surgeon.java
@@ -1,4 +1,33 @@
-public class Surgeon {
+public class Surgeon extends HealthProfessional {
+    private String surgeryType; 
+
+    // Default constructor
+    public Surgeon() {
+        super();
+        this.surgeryType = "General Surgery";
+    }
+
+    // Parameterized constructor
+    public Surgeon(int id, String name, String profession, String surgeryType) {
+        super(id, name, profession);
+        this.surgeryType = surgeryType;
+    }
+
+    // Override the method for printing details
+    @Override
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Surgery Type: " + surgeryType);
+    }
+
+    // Getter and Setter method
+    public String getSurgeryType() {
+        return surgeryType;
+    }
+
+    public void setSurgeryType(String surgeryType) {
+        this.surgeryType = surgeryType;
+    }
     
 
 }
