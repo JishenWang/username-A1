b9eec0ab418d4ede613b9d2944ab6f6798291d03
JishenWang <1801076890@qq.com>
2024-11-15 13:39:01 +0800

    5
    Create a new class for Appointment, creating instance variables "patientName, patientcall, preferredTimeSlot, selectedDoctor"

---
+++ src/Appointment.java
@@ -0,0 +1,30 @@
+public class Appointment {
+        private final String patientName;
+        private final String patientcall;
+        private final String preferredTimeSlot;
+        private final HealthProfessional selectedDoctor;
+    
+        // Default Constructor
+        public Appointment() {
+            this.patientName = "Unknown";
+            this.patientcall = "000-0000-0000";
+            this.preferredTimeSlot = "00:00";
+            this.selectedDoctor = null;
+        }
+    
+        // Constructor with parameters
+        public Appointment(String patientName, String patientMobile, String preferredTimeSlot, HealthProfessional selectedDoctor) {
+            this.patientName = patientName;
+            this.patientcall = patientMobile;
+            this.preferredTimeSlot = preferredTimeSlot;
+            this.selectedDoctor = selectedDoctor;
+        }
+    
+        // Method to print appointment details
+        public void printDetails() {
+            System.out.println("Patient Name: " + patientName);
+            System.out.println("Call: " + patientcall);
+            System.out.println("Preferred Time: " + preferredTimeSlot);
+        }
+
+}
